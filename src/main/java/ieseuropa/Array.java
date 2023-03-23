378d6ecfea08ff5df129b9f806e8c04ad4f40e54
OscarDH <chusmita.oscar@gmail.com>
2023-03-23 20:53:56 +0100

    Minimo y maximo Array

--- src/main/java/ieseuropa/Array.java
+++ src/main/java/ieseuropa/Array.java
@@ -2,9 +2,29 @@ package main.java.ieseuropa;
 
 public class Array {
 
+	private static int calcularMinimo(int[] array) {
+		int min = array[1];
+		for (int numero : array) {
+			if (numero < min)
+				min = numero;
+		}
+		return min;
+	}
+
+	private static int calcularMaximo(int[] array) {
+		int max = 0;
+		for (int numero : array) {
+			if (numero > max)
+				max = numero;
+		}
+		return max;
+	}
+
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Hola");
+		int[] array = { 1, 2, 3, 4, 5, 6, 7 };
+		System.out.println(calcularMinimo(array));
+		System.out.println(calcularMaximo(array));
+
 	}
 
 }
