b5fffb69e98e54f75805c585754f3bd651247c22
dev <dev@debian>
2023-03-23 18:00:48 +0100

    Calcular longitud

--- src/main/java/ieseuropa/Strings.java
+++ src/main/java/ieseuropa/Strings.java
@@ -1,10 +1,15 @@
-package main.java.ieseuropa;
 
-public class Strings {
+	package main.java.ieseuropa;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public class Strings {
+		
+		private static int CalcularLongitud(String cadena) {
+			return cadena.length();
+		}
+		
+		public static void main(String[] args) {
+			// TODO Auto-generated method stub
+		
+		}
 
 	}
-
-}
