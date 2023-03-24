4aec23124b8f4b348670264bf9e2ed90f825a45e
Sergioac52 <sergio.antona1@educa.madrid.org>
2023-03-25 00:16:22 +0100

    Añadido repetirLetras

--- src/main/java/ieseuropa/Strings.java
+++ src/main/java/ieseuropa/Strings.java
@@ -5,11 +5,17 @@ public class Strings {
 	private static String borrarApariencias(String cadena, String borrar) {
 		return cadena.replace(borrar, "");
 	}
-	
+	private static String repetirLetras(String cadena) {
+		String salida = "";
+		for (int i = 0; i < cadena.length(); i++) 
+			salida += cadena.charAt(i)+""+cadena.charAt(i);
+		return salida;
+	}
 	
 	public static void main(String[] args) {
 		
 		System.out.println(borrarApariencias("Real Zaragoza", "Real"));
+		System.out.println(repetirLetras("La casa del lago"));
 	}
 
 }
