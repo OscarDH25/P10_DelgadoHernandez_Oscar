ea9432a10497f7d38e0d719a2b48259a50967f26
SergioAC52 <sergio.antona1@educa.madrid.org>
2023-03-24 13:14:21 +0100

    Añadido ecuacion de segundo grado

--- src/main/java/ieseuropa/Decimales.java
+++ src/main/java/ieseuropa/Decimales.java
@@ -1,10 +1,17 @@
 package main.java.ieseuropa;
 
 public class Decimales {
-
+	
+	private static float[] ecuacion2Grado(float a, float b , float c) {
+		float raiz = (float) Math.sqrt(Math.pow(b, 2) -4 *a *c);
+		
+		return new float[] {(-b+raiz)/(2*a) ,(-b-raiz)/(2*a)};
+	}
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		float ecuacion[] = ecuacion2Grado(2f, -7f, 3f);
+		System.out.println("Solucion x1:"+ecuacion[0]+ " Solucion x2:"+ecuacion[1]);
 	}
 
 }
