6ef50b86b0f2cadc3e249f3d6b100e4685faa716
NeilyGuerra <niyeing07@gmail.com>
2023-03-27 18:20:47 +0200

    actualizado ordenar array

--- src/main/java/ieseuropa/Array.java
+++ src/main/java/ieseuropa/Array.java
@@ -1,5 +1,8 @@
 package main.java.ieseuropa;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Array {
 
 	private static int calcularMinimo(int[] array) {
@@ -19,11 +22,29 @@ public class Array {
 		}
 		return max;
 	}
+	private static void OrdenarArray() {
+		Scanner sc = new Scanner(System.in);
+
+		int[] num = new int[10];
+		int i;
+		for (i = 0; i < 10; i++) {
+		}
+		System.out.println("Introduce los elementos de tu array");
+		for (i = 0; i < 10; i++) {
+			num[i] = sc.nextInt();
+		}
+		Arrays.sort(num);
+		for (i = 0; i < 10; i++) {
+			System.out.print(num[i] + " ");
+		}
+	}
+
 
 	public static void main(String[] args) {
 		int[] array = { 1, 2, 3, 4, 5, 6, 7 };
 		System.out.println(calcularMinimo(array));
 		System.out.println(calcularMaximo(array));
+		OrdenarArray();
 
 	}
 
